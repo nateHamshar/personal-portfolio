e0745681ffd2d0ef8708795afed8caaeb7ff655f
Nate Hamshar <williamhamshar@gmail.com>
2022-06-12 09:10:42 -0400

    home page looks pretty good for now, next will probably be the about page

--- src/Home.js
+++ src/Home.js
@@ -4,9 +4,14 @@ const Home = () => {
     return (
     <div className="Home">
     <div className="homePage">
-        <div className="square">awd</div>
-        <div className="square">awd</div>
-        <div className="square">awd</div>
+        <div className="square one"></div>
+        <div className="square two"></div>
+        <div className="square three"></div>
+        <div className="square five"></div>
+        <div className="square four">
+            <p className="name">Nate Hamshar</p>
+            <p className="description">Front End Developer</p>
+        </div>
     </div>
     </div>
     );
