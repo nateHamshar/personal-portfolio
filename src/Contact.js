a2b209969a96e6481b2458902e38d6e42e988144
Nate Hamshar <williamhamshar@gmail.com>
2022-06-13 08:10:35 -0400

    contact page complete, not going to do the project page yet since the layout will depend on the project i end up adding

--- src/Contact.js
+++ src/Contact.js
@@ -1,6 +1,32 @@
+import "./Contact.css"
+
 const Contact = () => {
     return (
-        <h1>This will be the contact page</h1>
+        <div className="contactContainer">
+            <div class="formBox">  
+  <form id="contact" action="https://formspree.io/f/xvolyvrj" method="post">
+    <h3>Let's get in touch</h3>
+    <fieldset>
+      <input placeholder="Your name" type="text" tabindex="1" required autofocus/>
+    </fieldset>
+    <fieldset>
+      <input placeholder="Your Email Address" type="email" tabindex="2" required/>
+    </fieldset>
+    <fieldset>
+      <input placeholder="Your Phone Number (optional)" type="tel" tabindex="3"/>
+    </fieldset>
+    <fieldset>
+      <input placeholder="Your Web Site (optional)" type="url" tabindex="4" />
+    </fieldset>
+    <fieldset>
+      <textarea placeholder="Type your message here...." tabindex="5" required></textarea>
+    </fieldset>
+    <fieldset>
+      <button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Submit</button>
+    </fieldset>
+  </form>
+</div>
+        </div>
     );
 }
  
